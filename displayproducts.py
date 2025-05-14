cfe7069b0335041ca3a239316d9b540c65aa7a52
Raul Casian Bozan <raulcasian@Rauls-MacBook-Air-2.local>
2025-05-15 09:59:07 +1000

    Removed display.orders and added it inside the display.products so its together

--- displayproducts.py
+++ displayproducts.py
@@ -10,6 +10,13 @@ def displayProducts():
     conn.close()
     return render_template('displayproducts.html', allproducts=allproducts)
 
+@app.route('/displayorders', methods=['GET'])
+def displayOrders():
+    conn = get_db_connection()
+    allorders = conn.execute('SELECT * FROM orders').fetchall()
+    conn.close()
+    return render_template('displayorders.html', allorders=allorders)
+
 def get_db_connection():
     conn = sqlite3.connect('mydb.db')
     conn.row_factory = sqlite3.Row
